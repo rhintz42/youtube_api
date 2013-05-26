5629a2dc04d312ff9d4579fd36a1ed0b51abbfdd
Robert Hintz <rhintz42@stanford.edu>
2013-05-26 01:39:02 -0700

    Add initial files for youtube api applicaiton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTestYoutubeApi::Application.config.secret_token = '9d750ffeb799ed1318d9d36ee969d05cd12a48fa5c6fdf4fd2908bbc906023c66965eb337ce2f9b3070dee0c5f66af785dbda9db7104673835887990ba3803cd'
